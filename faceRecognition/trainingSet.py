90917d4483c57f54519f8c9f8e63038a629a389b
David Rower <ki6pmp@gmail.com>
2017-01-08 13:37:41 -0800

    modifying code for multiple people

--- faceRecognition/trainingSet.py
+++ faceRecognition/trainingSet.py
@@ -10,7 +10,7 @@
 
 from glob import glob
 import sys, os
-import cv2, Image
+import cv2
 import numpy as np
 
 # facial detection using haarcascades from openCV
@@ -28,31 +28,40 @@ def detectFaces(image,faceCascade):
 def checkDirectory():
     fullPath  = os.path.realpath(__file__)
     directory = os.path.dirname(fullPath)
-    cascade   = glob("{}/*.xml".format(directory))
-    images, extensions = [], ['jpg','png']
-    for extension in extensions: 
-        images.extend(glob("{0}/*.{1}".format(directory,extension)))
+    people    = glob("{}/*/".format(directory))
+    cascade   = glob("{}/haarcascade_frontalface_*.xml".format(directory))
+    images, extensions = {}, ['jpg','png']
+    for person in people: 
+        imageList = []
+        for extension in extensions: 
+            imageList.extend(glob("{0}/*.{1}".format(person,extension)))
+        images["{}".format(person)] = imageList
     return directory, cascade, images
 
 # iterate through image path names, append faces to a training set
 def createTrainingSet(images,faceCascade):
-    trainingSet = []
-    for imagePath in images:
-        color = cv2.imread(imagePath)
-        gray  = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-        faces = detectFaces(gray,faceCascade)  
-        print("Faces found: {}".format(len(faces)))
-        for (x,y,w,h) in faces: 
-            cv2.rectangle(color, (x,y), (x+w,y+h), (100,255,0),2)
-            cv2.imshow('Face', color)
-            cv2.waitKey(100)
-            trainingSet.append(cv2.equalizeHist(gray[x:x+w,y:y+h]))
-    return trainingSet
+    index, trainingSet, labels = 0, [], []
+    for person, imageList in images.iteritems():
+        for imagePath in imageList:
+            color = cv2.imread(imagePath)
+            gray  = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+            gray  = cv2.equalizeHist(gray)
+            faces = detectFaces(gray,faceCascade)  
+#            print("Faces found: in {0}: {1}".format(imagePath,len(faces)))
+            for (x,y,w,h) in faces: 
+                cv2.imshow('Face', gray[y:y+h,x:x+w])
+                cv2.waitKey(100)
+                trainingSet.append(gray[y:y+h,x:x+w])
+                labels.append(index)
+        index += 1
+    return trainingSet, labels
 
-if __name__ == "__main__":
+if __name__ == "__main__":       
     # find directory and important file paths
     directory, cascade, images = checkDirectory()
+    names = [key.split('/')[-2] for key in images.keys()]
     print("Directory: {}/".format(directory))
+    print("People: {}".format(names))
 
     # quits if no cascade or image files are found
     if not cascade: 
@@ -62,15 +71,34 @@ if __name__ == "__main__":
 
     # creates the cascade
     faceCascade = cv2.CascadeClassifier(cascade[0])
-
     print("Cascade used: {}".format(cascade[0]))
-    print("Training set: {}".format(len(images)))
-    
-    label = raw_input("Label? ")
-    trainingSet = createTrainingSet(images,faceCascade)
+
+    # creates the training set given dictionary of image file paths
+    trainingSet, labels = createTrainingSet(images,faceCascade)
+    for label in np.unique(labels): 
+        imageNumber = labels.count(label)
+        print("{}: {} training images".format(names[label],imageNumber))
     cv2.destroyAllWindows()
 
-    
+    faceRecognizer = cv2.createLBPHFaceRecognizer()
+    faceRecognizer.train(trainingSet,np.array(labels))
+
+    color = cv2.imread(images[0])
+    print("Test on {}".format(images[0]))
+    gray  = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    gray  = cv2.equalizeHist(gray)
+    faces = detectFaces(gray,faceCascade) 
+    for (x,y,w,h) in faces: 
+        prediction, confidence = faceRecognizer.predict(gray[y:y+h,x:x+w])
+        print("Prediction: {0}, Confidence: {1}".format(prediction,confidence))
+
+
+
+
+
+
+
+
 
              
 
